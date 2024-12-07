f3d30f63ce073517fe770db51f87ddfe867b8dec
lynaxpy <142473397+lynaxpy@users.noreply.github.com>
2024-12-07 20:12:09 +0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,14 +1,12 @@
 
 #!/bin/bash
 
-# Форматирование разделов
-echo "==> Форматирование разделов"
+echo "Formating partions"
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
 mkfs.ext4 /dev/sda3
 
-# Монтирование разделов
-echo "==> "
+echo "Mounting partions"
 rm -rf /mnt
 mkdir /mnt
 mount /dev/sda2 /mnt
@@ -17,37 +15,37 @@ mkdir /mnt/home
 mount /dev/sda1 /mnt/boot
 mount /dev/sda3 /mnt/home
 
-echo "==> Installing basic system"
+echo "Installing basic system"
 pacstrap /mnt base base-devel linux linux-firmware 
 
-echo "==> Generetaing fstab"
+echo "Generetaing fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "==> Moving to chroot env"
+echo "Moving to chroot env"
 arch-chroot /mnt /bin/bash <<EOF
 
 
-echo "==> Installing and setting up bootloader"
+echo "Installing and setting up bootloader"
 pacman -S --noconfirm grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "==> Creating Users"
+echo "Creating Users"
 useradd -m -G wheel -s /bin/bash lynax
 echo "lynax:password" | chpasswd
 echo "root:password" | chpasswd
 
-echo "==> Setting up sudo"
+echo "Setting up sudo"
 sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
-echo "==> Installing GDM and NetworkManager"
-pacman -S --noconfirm gdm gnome networkmanager hyprland kitty net-tools nano intel-ucode pipewire pipewire-pulse pipewire-alsa pipewire-jack git bluez bluez-utils
+echo "Installing GDM and NetworkManager"
+pacman -S --noconfirm gdm gnome networkmanager hyprland kitty net-tools nano intel-ucode git bluez bluez-utils firefox
 
 systemctl enable gdm
 systemctl enable NetworkManager
 
 EOF
 
-echo "==> Installing finished! Reboot system"
+echo "Installing finished! Reboot system"
 
 
